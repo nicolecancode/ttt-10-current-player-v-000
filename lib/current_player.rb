0e5a308a876eae7b4b8d3a45ef2140fed3b06e5b
Nicole Pretorius <nicole.pretoriusuk+learn@gmail.com>
2018-08-14 14:02:18 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,11 +2,18 @@ def turn_count(board)
   counter = 0
 
   board.each do |turn|
-    if turn == "x" || turn == "o"
+    if turn.downcase == "x" || turn.downcase == "o"
       counter += 1
       puts "#{counter}"
     end
   end
 
   counter
+end
+
+  def current_player(board)
+		if turn_count % 2
+		puts “x”
+  else “o”
+	end
 end
