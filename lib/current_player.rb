e038efe9124b18f4e108c28cd07e7d0575f4954d
Nicole Pretorius <nicole.pretoriusuk+learn@gmail.com>
2018-08-14 14:22:09 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -17,15 +17,3 @@ def current_player(board)
   end
   return player
 end
-
-
-
-  current_players = ["X", "O"]
-  current_players.each do |current_player|
-    if turn_count % 2 == 0
-      puts "X"
-    else
-      puts "O"
-    end
-  end
-end
