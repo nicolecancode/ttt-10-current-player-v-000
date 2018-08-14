701105f986d79d24a8de63e942e24963b3daf90a
Nicole Pretorius <nicole.pretoriusuk+learn@gmail.com>
2018-08-14 13:51:27 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,8 +2,9 @@ def turn_count(board)
 	def current_player(board)
 	counter = 8
 		if turn_count % 2
-		current_player = “X”
+		    current_player = “X”
 		else “O”
+    end
 	counter += 1
 	end
 end
